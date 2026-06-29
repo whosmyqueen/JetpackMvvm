101612568bb0318883c0c432a627f2bd19d7d787
whosmyqueen <644049260@qq.com>
2026-06-29 11:19:48 +0800

    config(build): 配置国内镜像仓库以加速依赖下载
    - 添加阿里云Maven仓库镜像地址
    - 添加华为云Maven仓库镜像地址
    - 添加JitPack仓库支持
    - 重新排序仓库配置顺序优化下载速度

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,13 @@
 @file:Suppress("UnstableApiUsage")
 pluginManagement {
     repositories {
+        // 优先使用国内镜像加速
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://repo.huaweicloud.com/repository/maven/")
+        maven("https://jitpack.io")
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -22,10 +29,10 @@ dependencyResolutionManagement {
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/central")
         maven("https://repo.huaweicloud.com/repository/maven/")
+        maven("https://jitpack.io")
         // 官方源
         google()
         mavenCentral()
-        maven("https://jitpack.io")
     }
 }
 
